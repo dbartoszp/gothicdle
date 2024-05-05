c8b2185af8b13d374315ab8b0c98193ddbfd4b27
dbartoszp <dbartosz.pomierny@wp.pl>
2024-05-05 19:59:31 +0200

    tmp

--- app/page.tsx
+++ app/page.tsx
@@ -4,39 +4,29 @@ import { Instructions } from '@/modules/MainGameCard/Instructions/Instructions';
 import { MainGameCard } from '@/modules/MainGameCard/MainGameCard';
 import { TitleHeader } from '@/modules/TitleHeader/TitleHeader';
 import { useGetCurrentCorrectCharacter } from '@/modules/characters/hooks/useGetCurrentCorrectCharacter/useGetCurrentCorrectCharacter';
-import { Button } from '@/modules/ui/Button/Button';
 import { Text } from '@/modules/ui/Text/Text';
 
 export default function Home() {
-	const currentCorrectCharacter = useGetCurrentCorrectCharacter();
+  const currentCorrectCharacter = useGetCurrentCorrectCharacter();
 
-	if (!currentCorrectCharacter.isSuccess) {
-		return <Text>ERROR</Text>;
-	}
+  if (!currentCorrectCharacter.isSuccess) {
+    return <Text>ERROR</Text>;
+  }
 
-	const currentCorrectCharacterData = currentCorrectCharacter.data;
+  const currentCorrectCharacterData = currentCorrectCharacter.data;
 
-	return (
-		<>
-			<main className="flex min-h-screen flex-col items-center space-y-12 pt-12">
-				{/* <Button
-					onClick={() =>
-						console.log(currentCorrectCharacterData.characterId)
-					}
-				>
-					charId: {currentCorrectCharacterData.characterId}
-				</Button> */}
-				<TitleHeader />
-				{currentCorrectCharacterData && (
-					<MainGameCard
-						correctCharacterId={
-							currentCorrectCharacterData.characterId
-						}
-					/>
-				)}
-				<Instructions />
-			</main>
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <main className='flex min-h-screen flex-col items-center space-y-12 pt-12'>
+        <TitleHeader />
+        {currentCorrectCharacterData && (
+          <MainGameCard
+            correctCharacterId={currentCorrectCharacterData.characterId}
+          />
+        )}
+        <Instructions />
+      </main>
+      <Footer />
+    </>
+  );
 }
