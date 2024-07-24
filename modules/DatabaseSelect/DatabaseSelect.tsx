6fd68d3e75cd60e2ed3b4946fd63c087fc70c326
dbartoszp <dbartosz.pomierny@wp.pl>
2024-07-24 16:08:57 +0200

    feat: database selection done (classic)

--- modules/DatabaseSelect/DatabaseSelect.tsx
+++ modules/DatabaseSelect/DatabaseSelect.tsx
@@ -31,26 +31,88 @@ export const DatabaseSelect = ({
         open={isOpen}
         onOpenChange={changeOpenState}
       >
-        <div className='flex flex-col text-center'>
-          <>
-            <Link
-              size='sm'
-              href={`http://localhost:3000/${isEndless ? 'endless' : 'classic'}?database=wybrane`}
+        <div className='m-auto mt-6 flex flex-col justify-center space-y-3 text-center md:max-w-xs '>
+          <Link
+            size='sm'
+            href={`http://localhost:3000/${isEndless ? 'endless' : 'classic'}?database=wybrane`}
+          >
+            <Text
+              variant={
+                currentDatabase.toLocaleLowerCase() === 'wybrane'
+                  ? 'greenSm'
+                  : 'small'
+              }
             >
-              <Text
-                variant={
-                  currentDatabase.toLocaleLowerCase() === 'wybrane'
-                    ? 'greenSm'
-                    : 'small'
-                }
-              >
-                Wybrane
-              </Text>
-            </Link>
-            <Text>
-              Recznie wybrane, rozpoznawalne postacie ze wszystkich gier
+              Wybrane
             </Text>
-          </>
+          </Link>
+          <Text variant='small'>
+            Recznie wybrane, rozpoznawalne postacie ze wszystkich gier
+          </Text>
+          <Link
+            size='sm'
+            href={`http://localhost:3000/${isEndless ? 'endless' : 'classic'}?database=gothic`}
+          >
+            <Text
+              variant={
+                currentDatabase.toLocaleLowerCase() === 'gothic'
+                  ? 'greenSm'
+                  : 'small'
+              }
+            >
+              Gothic
+            </Text>
+          </Link>
+          <Text variant='small'>Wszystkie postacie z gry Gothic</Text>
+          <Link
+            size='sm'
+            href={`http://localhost:3000/${isEndless ? 'endless' : 'classic'}?database=gothic2`}
+          >
+            <Text
+              variant={
+                currentDatabase.toLocaleLowerCase() === 'gothic2'
+                  ? 'greenSm'
+                  : 'small'
+              }
+            >
+              Gothic 2 + NK
+            </Text>
+          </Link>
+          <Text variant='small'>
+            Wszystkie postacie z gry Gothic 2 + Nocy Kruka
+          </Text>
+          <Link
+            size='sm'
+            href={`http://localhost:3000/${isEndless ? 'endless' : 'classic'}?database=gothic3`}
+          >
+            <Text
+              variant={
+                currentDatabase.toLocaleLowerCase() === 'gothic3'
+                  ? 'greenSm'
+                  : 'small'
+              }
+            >
+              Gothic 3
+            </Text>
+          </Link>
+          <Text variant='small'>Wszystkie postacie z gry Gothic 3</Text>
+          <Link
+            size='sm'
+            href={`http://localhost:3000/${isEndless ? 'endless' : 'classic'}?database=wszystkie`}
+          >
+            <Text
+              variant={
+                currentDatabase.toLocaleLowerCase() === 'wszystkie'
+                  ? 'greenSm'
+                  : 'small'
+              }
+            >
+              Wszystkie
+            </Text>
+          </Link>
+          <Text variant='small'>
+            Wszystkie postacie z gier Gothic, Gothic 2 + NK i Gothic 3
+          </Text>
         </div>
       </Modal>
     </>
